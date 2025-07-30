e93675ef6a2eae9186ed437d4beb3ab451e5df9d
thisisjustausername <lpwgfs@gmail.com>
2025-07-30 22:36:18 +0200

    add testing.py in pdf_reader

---
+++ pdf_reader/testing.py
@@ -0,0 +1,10 @@
+from pdf_reader.MHB import MHB
+import pdf_reader.pdf_reader_toc as prt
+
+name = "pdfs/Modulhandbuch_Bachelor_of_Music_PO_2023_ID48735_5_de_20250414_1754.pdf"
+mhb = prt.Modules(name)
+modules = mhb.toc_module_codes()
+module_code = "MUS-5113"
+for i in modules:
+    info = mhb.data_to_module(i)
+    print(info["module_code"], info["title"])
