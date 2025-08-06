1ef620b28c8adc3638b545e79570e02765a146d0
thisisjustausername <lpwgfs@gmail.com>
2025-08-06 23:17:59 +0200

    add files

---
+++ extract_info/check_errors.py
@@ -0,0 +1,21 @@
+import json
+from pdf_reader.MHB import MHB
+
+import traceback
+
+with open("uni_augsburg_error_files.json", "r") as file:
+    data = json.load(file)
+
+with open("web_scraping/scrape_uni_augsburg/links_information.json", "r") as file:
+    links_data =  json.load(file)
+
+look_up = [(k, v) for k, v in links_data.items()]
+
+for i in data:
+    # print(next(k for k, v in look_up if v == i))
+    try:
+        MHB("pdfs/" + i)
+        print("SUCCESS")
+    except Exception as e:
+        # traceback.print_exc()
+        print("ERROR")        
---
+++ extract_info/extract_info_augsburg.py
@@ -0,0 +1,49 @@
+from pdf_reader.MHB_Overlaps import Overlaps
+from pdf_reader.MHB import MHB
+import json
+import os
+
+mhbs = os.listdir("pdfs")
+
+length = len(mhbs)
+
+i = mhbs[0]
+
+def dictToTuple(d: dict[str, str | int | None]):
+    return tuple([(k, v if type(v) != list else tuple(v)) for k, v in d.items()])
+
+def tupleToDict(t: tuple):
+    return {k: v for k, v in t}
+
+mhb = MHB("pdfs/" + i)
+
+all_mhbs= []
+error_files = []
+look_up_dict = {}
+
+
+for index, i in enumerate(mhbs):
+    print(f"{index+1} / {length}")
+    try:
+        mhb = MHB("pdfs/" + i)
+    except (IndexError, AssertionError, ValueError):
+        error_files.append(i)
+        continue
+    mhb_dict = {"title": mhb.title, "modules": mhb.modules, "module_codes": mhb.module_codes}
+    look_up_dict[i] = mhb_dict
+    all_mhbs += mhb_dict["modules"]
+
+tupled_data = [dictToTuple(i) for i in all_mhbs] # type: ignore
+
+clean_modules = list(set(tupled_data))
+
+mhbs = [tupleToDict(i) for i in clean_modules] # type: ignore
+
+with open("uni_augsburg_module_data.json", "w") as file:
+    json.dump(mhbs, file, indent=3)
+
+with open("web_service/uni_augsburg_look_up.json", "w") as file:
+    json.dump(look_up_dict, file, indent=3)
+
+with open("uni_augsburg_error_files.json", "w") as file:
+    json.dump(error_files, file, indent=3)
