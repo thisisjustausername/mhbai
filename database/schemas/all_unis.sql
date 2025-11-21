6c3967dd441e707c2fb53b4d17c64ec88e24b4f3
thisisjustausername <lpwgfs@gmail.com>
2025-11-21 13:48:58 +0100

    create schemas

--- database/schemas/all_unis.sql
+++ database/schemas/all_unis.sql
@@ -3,25 +3,67 @@
 -- This file is part of mhbai.
 
 -- Licensed under the AGPL-3.0 License. See LICENSE file in the project root for full license information.--
--- Description: crawl a web page and return all the urls it contains
+-- Description: create schema, tables and triggers for all universities
 -- STATUS: VERSION 1.0
--- FileID: Sc-da-0002
+-- FileID: Da-sc-0002
 
-CREATE OR REPLACE FUNCTION check_inserted_vals()
+
+-- create schema for university of augsburg
+
+CREATE schema IF NOT EXISTS all_unis
+AUTHORIZATION gatterle;
+
+
+-- create tables
+
+CREATE TABLE IF NOT EXISTS all_unis.universal_mhbs (
+    id SERIAL PRIMARY KEY,
+    source_url TEXT NOT NULL UNIQUE,
+    source_title TEXT,
+    source TEXT, 
+    name TEXT, 
+    city TEXT, 
+    university TEXT, 
+    degree TEXT, 
+    uni_url TEXT,
+    search_string TEXT, 
+    additional_info JSONB
+);
+
+CREATE TABLE IF NOT EXISTS all_unis.universities (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    city TEXT,
+    website TEXT UNIQUE, --- technically also not UNIQUE for same reason as mhb_url, but might be changed before being used
+    mhb_url TEXT, -- for now not unique since handling same university from different city separately UNIQUE,
+    label_mhb_good BOOLEAN, 
+    label_mhb_manually BOOLEAN DEFAULT FALSE,
+    UNIQUE (name, city)
+);
+
+
+-- create triggers
+
+-- trigger to add university to universities table when inserting mhbs into universal_mhbs
+CREATE OR REPLACE FUNCTION add_university()
     RETURNS TRIGGER AS
 $$
-DECLARE
-    vals TEXT[3] := ARRAY [NEW.name, NEW.university, NEW.degree, NEW.city]; -- NEW.uni_url removed since added after fetching base data
 BEGIN
-    IF (SELECT COUNT(*) FROM unnest(vals) AS col WHERE col IS NOT NULL) > 0 AND
-       (SELECT COUNT(*) FROM unnest(vals) AS col WHERE col IS NULL) > 0 THEN
-        RAISE EXCEPTION 'If one of title, uni_url, or search_string is provided, all three must be provided.';
-    END IF;
+    INSERT INTO universities (name, city)
+    VALUES (NEW.university, NEW.city)
+    ON CONFLICT (name, city) DO NOTHING;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE TRIGGER add_university_trigger
+    BEFORE INSERT OR UPDATE
+    ON all_unis.universal_mhbs
+    FOR EACH ROW
+EXECUTE FUNCTION add_university();
 
+
+-- trigger to create search string before inserting or updating universal mhbs
 CREATE OR REPLACE FUNCTION add_search_string()
     RETURNS TRIGGER AS
 $$
@@ -31,7 +73,7 @@ BEGIN
         RETURN NEW; -- no changes, do nothing
     END IF;
 
-    NEW.search_string := RTRIM(CONCAT('page:', NEW.uni_url, ' filetype:pdf modulhandbuch ', NEW.name, ' ', COALESCE(
+    NEW.search_string := RTRIM(CONCAT('site:', NEW.uni_url, ' filetype:pdf modulhandbuch ', NEW.name, ' ', COALESCE(
                                                                                                                        CASE
                                                                                                                            WHEN NEW.degree IS NOT NULL AND NEW.degree ILIKE '%bachelor%'
                                                                                                                                THEN 'Bachelor'
@@ -52,29 +94,30 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
-CREATE OR REPLACE TRIGGER check_inserted_vals_trigger
-    BEFORE INSERT
-    ON universal_mhbs
+CREATE OR REPLACE TRIGGER add_search_string_trigger
+    BEFORE INSERT OR UPDATE
+    ON all_unis.universal_mhbs
     FOR EACH ROW
-EXECUTE FUNCTION check_inserted_vals();
+EXECUTE FUNCTION add_search_string();
 
 
-
-CREATE OR REPLACE FUNCTION add_university()
+-- trigger to check that if one of name, uni_url, or search_string is provided, all three are provided
+CREATE OR REPLACE FUNCTION check_inserted_vals()
     RETURNS TRIGGER AS
 $$
+DECLARE
+    vals TEXT[3] := ARRAY [NEW.name, NEW.university, NEW.degree, NEW.city]; -- NEW.uni_url removed since added after fetching base data
 BEGIN
-    INSERT INTO universities (name, city)
-    VALUES (NEW.university, NEW.city)
-    ON CONFLICT (name, city) DO NOTHING;
+    IF (SELECT COUNT(*) FROM unnest(vals) AS col WHERE col IS NOT NULL) > 0 AND
+       (SELECT COUNT(*) FROM unnest(vals) AS col WHERE col IS NULL) > 0 THEN
+        RAISE EXCEPTION 'If one of title, uni_url, or search_string is provided, all three must be provided.';
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-
-CREATE OR REPLACE TRIGGER add_university_trigger
-    BEFORE INSERT OR UPDATE
-    ON universal_mhbs
+CREATE OR REPLACE TRIGGER check_inserted_vals_trigger
+    BEFORE INSERT
+    ON all_unis.universal_mhbs
     FOR EACH ROW
-EXECUTE FUNCTION add_university();
+EXECUTE FUNCTION check_inserted_vals();
---
+++ database/schemas/unia.sql
@@ -0,0 +1,41 @@
+-- Copyright (c) 2025 Leon Gattermeyer
+
+-- This file is part of mhbai.
+
+-- Licensed under the AGPL-3.0 License. See LICENSE file in the project root for full license information.--
+-- Description: create schema, tables and triggers for university of augsburg
+-- STATUS: VERSION 1.0
+-- FileID: Da-sc-0001
+
+
+-- create schema for university of augsburg
+
+CREATE schema IF NOT EXISTS unia
+AUTHORIZATION gatterle;
+
+
+-- create tables
+
+CREATE TABLE IF NOT EXISTS unia.mhbs (
+    id SERIAL PRIMARY KEY,
+    web_url TEXT NOT NULL UNIQUE, 
+    folder TEXT NOT NULL, 
+    pdf_name TEXT,
+    title TEXT);
+
+CREATE TABLE IF NOT EXISTS unia.modules (
+    id SERIAL PRIMARY KEY,
+    module_code TEXT UNIQUE NOT NULL, 
+    content TEXT, 
+    goals TEXT, 
+    title TEXT NOT NULL, 
+    ects INTEGER
+    );
+
+CREATE TABLE IF NOT EXISTS unia.mhbs_modules_link (
+    id SERIAL PRIMARY KEY,
+    mhb_id INTEGER REFERENCES unia.mhbs(id) ON DELETE CASCADE,
+    module_id INTEGER REFERENCES unia.modules(id) ON DELETE CASCADE,
+    pages JSONB, 
+    UNIQUE (mhb_id, module_id)
+    );
