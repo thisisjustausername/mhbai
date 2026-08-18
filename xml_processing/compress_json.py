cf0a96ebf4307097df473772a94fc8e484cccc2c
thisisjustausername <lpwgfs@gmail.com>
2026-08-18 20:02:32 +0200

    fix: catch all NA strings, remove join for lists to string

--- xml_processing/compress_json.py
+++ xml_processing/compress_json.py
@@ -31,6 +31,7 @@ def compress_json(local_path: Path) -> dict[str, Any] | None:
     if data is None:
         return None
 
+    recursive_walk(data, remove_keys = [], rename_keys = {}, retype_values={}, replace_values = {"keine Angabe": None, 'NA': None})
     mhb = {}
     mhb["path"] = str(local_path)
     mhb["name"] = data.get("name", None)
@@ -73,10 +74,10 @@ def compress_json(local_path: Path) -> dict[str, Any] | None:
             module["goals"] = None if (a := re.sub(r'\s+', ' ', (j.get("goals", "") or "")).strip()) == "" else a
             module["international"] = True if j.get("international", None) == 1 else False if j.get("international", None) == 0 else None
             module["prerequisite_language"] = j.get("prerequisite_language", None)
-            langs = {e for e in [e.get("language", None) for e in j.get("languages", [])] if e is not None}
-            module["languages"] = ", ".join(langs) if len(langs) > 0 else None
-            start_sem = j.get("available_semesters", {}).get("start_semester", {}).get("name", "NA")
-            end_sem = j.get("available_semesters", {}).get("end_semester", {}).get("name", "NA")
+            langs = sorted({e for e in [e.get("language", None) for e in j.get("languages", [])] if e is not None})
+            module["languages"] = langs if len(langs) > 0 else None
+            start_sem = j.get("available_semesters", {}).get("start_semester", {}).get("name", None)
+            end_sem = j.get("available_semesters", {}).get("end_semester", {}).get("name", None)
             module["available_semesters"] = {"start_semester": start_sem, "end_semester": end_sem, "frequency": j.get("frequency", [{}])[0].get("frequency_name", None)}
             module["keywords"] = j.get("keywords", None)
             usability = j.get("usability", [])
@@ -97,13 +98,13 @@ def compress_json(local_path: Path) -> dict[str, Any] | None:
                 exam["name"] = e.get("name", None)
                 exam["duration"] = (str(e.get("duration", None)) + ((" " + e.get("time_unit", None)) if e.get("time_unit", None) is not None else "")) if e.get("duration", None) is not None else None
                 exam["graded"] = e.get("graded", None)
-                exam["portion_of_grade"] = e.get("portion_of_grade", None) if e.get("portion_of_grade", None) is not None else "NA"
+                exam["portion_of_grade"] = e.get("portion_of_grade", None)
                 exam["preparation"] = e.get("preparation", None)
                 exam["deadline"] = e.get("deadline", None)
                 exam["description"] = e.get("description", None)
                 exam["type"] = e.get("type", None)
-                langs = {a for a in [a.get("language", None) for a in e.get("languages", [])] if a is not None}
-                exam["languages"] = ", ".join(langs) if len(langs) > 0 else None
+                langs = sorted({a for a in [a.get("language", None) for a in e.get("languages", [])] if a is not None})
+                exam["languages"] = langs if len(langs) > 0 else None
                 freq = e.get("exam_frequency", [{}])
                 if isinstance(freq, dict):
                     freq = [freq]
@@ -129,14 +130,14 @@ def compress_json(local_path: Path) -> dict[str, Any] | None:
                     exam["name"] = l.get("name", None)
                     exam["duration"] = str(l.get("duration", None)) + ((" " + l.get("time_unit", None)) if l.get("time_unit", None) is not None else "")
                     exam["graded"] = l.get("graded", None)
-                    exam["portion_of_grade"] = l.get("portion_of_grade", None) if l.get("portion_of_grade", None) is not None else "NA"
+                    exam["portion_of_grade"] = l.get("portion_of_grade", None)
                     exam["preparation"] = l.get("preparation", None)
                     exam["deadline"] = l.get("deadline", None)
                     description = l.get("description", None)
                     exam["description"] = description if not isinstance(description, dict) else description.get('html', {}).get('body', None)
                     exam["type"] = l.get("type", None)
-                    langs = {a for a in [a.get("language", None) for a in l.get("languages", [])] if a is not None}
-                    exam["languages"] = ", ".join(langs) if len(langs) > 0 else None
+                    langs = sorted({a for a in [a.get("language", None) for a in l.get("languages", [])] if a is not None})
+                    exam["languages"] = langs if len(langs) > 0 else None
                     freq = l.get("exam_frequency", [{}])
                     if isinstance(freq, dict):
                         freq = [freq]
@@ -166,11 +167,11 @@ def compress_json(local_path: Path) -> dict[str, Any] | None:
                 goals = k.get("goals", "") if isinstance(k.get("goals", ""), str) else ""
 
                 part["goals"] = None if (a := re.sub(r'\s+', ' ', (goals).strip())) == "" else a
-                langs = {a for a in [a.get("name", None) for a in k.get("languages", [])] if a is not None}
-                part["languages"] = ", ".join(langs) if len(langs) > 0 else None
+                langs = sorted({a for a in [a.get("name", None) for a in k.get("languages", [])] if a is not None})
+                part["languages"] = langs if len(langs) > 0 else None
                 tms = [l.get("name", None) for l in (k.get("teaching_methods", []) or []) if l is not None]
                 tms = [l for l in tms if l is not None]
-                part["teaching_methods"] = ", ".join(tms) if len(tms) > 0 else None
+                part["teaching_methods"] = tms if len(tms) > 0 else None
                 part["lecturer"] = k.get("lecturer", module["lecturer"]) # TODO check whether that is good
                 part["workloads"] = k.get("workloads", None)
                 part["subsubparts"] = [{
@@ -189,7 +190,6 @@ def compress_json(local_path: Path) -> dict[str, Any] | None:
             module_group["modules"].append(module)
         mhb["module_groups"].append(module_group)
 
-    recursive_walk(mhb, remove_keys = [], rename_keys = {}, retype_values={}, replace_values = {"keine Angabe": None, 'NA': None})
     return mhb
 
 
