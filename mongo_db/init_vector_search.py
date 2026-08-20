2dce081481ba7798f288a544cfe6c0e0bac76f41
thisisjustausername <lpwgfs@gmail.com>
2026-08-20 22:51:47 +0200

    feat: embed all wished fields, add filters

--- mongo_db/init_vector_search.py
+++ mongo_db/init_vector_search.py
@@ -16,6 +16,17 @@ client = MongoClient(
 )
 
 collections = ['mhbs', 'modules', 'exams']
+# TODO: maybe only show partial path for security reasons
+embedded_keys = {'mhbs': ['description', 'faculties', 'name', 'path'],
+                 'modules': ['content', 'goals', 'exam_outline', 'faculty_chair', 'lecturer', 'name', 'prerequisites', 'success_requirements', 'workloads'],
+                 'exams': ['description', 'duration', 'frequency', 'name', 'preparation', 'type']}
+
+embedded_keys_paths = {key: ['embedding_' + e for e in value] + ['embedding'] for key, value in embedded_keys.items()}
+
+filter_keys = {'mhbs': ['start_semester'],
+              'modules': ['available_semesters', 'ects', 'exams', 'international', 'languages', 'mandatory', 'module_code', 'recommended_semester_span', 'weekly_hours', 'workload_hours'],
+              'exams': ['deadline', 'graded', 'id', 'portion_of_grade']}
+
 
 for collection in collections:
     coll = client['unia'][collection]
@@ -24,13 +35,17 @@ for collection in collections:
         'type': 'vectorSearch',
         'definition': {
             'fields': [
-                {
+                *[{
                     'type': 'vector',
-                    'path': 'embedding',
+                    'path': key,
                     # NOTE: match this to the model using: bash: ollama show qwen3-embedding | grep 'embedding length' (for qwen3 embeddings)
                     'numDimensions': 4096,
                     'similarity': 'cosine',
-                }
+                } for key in embedded_keys_paths[collection]],
+                *[{
+                    'type': 'filter',
+                    'path': key
+                } for key in filter_keys[collection] + ['_hash']]
             ]
         }
     })
